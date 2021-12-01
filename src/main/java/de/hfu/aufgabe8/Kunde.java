149d4d9f794b374a545791ee00dc49622cc08e08
Maximilian Maes <maximilian.maes@aesculap.de>
2021-12-01 12:40:40 +0100

    Konstruktor Kunde Testkunde angelegt über Main

--- src/main/java/de/hfu/aufgabe8/Kunde.java
+++ src/main/java/de/hfu/aufgabe8/Kunde.java
@@ -1,11 +1,28 @@
 package de.hfu.aufgabe8;
 
+import java.util.Date;
+
 public class Kunde extends Nutzer {
 
-	private String strasse;
-	private int hausnummer;
-	private String ort;
-	private String plz;
-	private String telefonnummer;
+    private String strasse;
+    private int hausnummer;
+    private String ort;
+    private String plz;
+    private String telefonnummer;
+
+    Kunde() {
+        super();
+    }
+
+    Kunde(String vorname, String nachname, String eMail, String benutzername,
+          String passwort, Date geburtstag, Fakultaet fakultaet,
+          String strasse, int hausnummer, String ort, String plz, String telefonnummer) {
+        super(vorname, nachname, eMail, benutzername, passwort, geburtstag, fakultaet);
+        this.strasse = strasse;
+        this.hausnummer = hausnummer;
+        this.ort = ort;
+        this.plz = plz;
+        this.telefonnummer = telefonnummer;
+    }
 
 }
--- src/main/java/de/hfu/aufgabe8/Main.java
+++ src/main/java/de/hfu/aufgabe8/Main.java
@@ -1,10 +1,22 @@
 package de.hfu.aufgabe8;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
+import java.util.Calendar;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
         Nutzerverwaltung nutzerverwaltung = new Nutzerverwaltung();
-        Medienverwaltung medienverwaltung  = new Medienverwaltung();
+        Medienverwaltung medienverwaltung = new Medienverwaltung();
+
+        nutzerverwaltung.nutzerHinzufuegen(
+                new Kunde("Max", "Mustermann",
+                        "mm@test.de", "maxmuster",
+                        "passwort", new Date(2000, Calendar.DECEMBER, 24),
+                        Fakultaet.Informatik, "Musterstrasse", 1,
+                        "Nordpol", "00001", "08002222222"));
+
+
+
+
     }
 }
