ce3c7190abc3e29968dddade29cbf09d7917dbc7
Maximilian Maes <maximilian.maes@aesculap.de>
2022-01-18 13:40:35 +0100

    Einfügen von User Dialog in Main
    Es fehlt die Funktionstüchtigkeit der Optionen

--- src/main/java/de/hfu/aufgabe8/Main.java
+++ src/main/java/de/hfu/aufgabe8/Main.java
@@ -1,6 +1,8 @@
 package de.hfu.aufgabe8;
 
+import javax.rmi.ssl.SslRMIClientSocketFactory;
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -58,6 +60,51 @@ public class Main {
         medienverwaltung.mediumAusleihen(medium8, kunde2);
         medienverwaltung.mediumAusleihen(medium9, kunde2);
         medienverwaltung.mediumAusleihen(medium10, kunde2);
+
+
+        while (true) {
+
+            System.out.println("--------------------------\n\n");
+
+            System.out.println("Was möchten sie tun?");
+            System.out.println("(1) User anlegen");
+            System.out.println("(2) Medien in Bestand erfassen");
+            System.out.println("(3) Ausleihen");
+            System.out.println("(4) Verlängern");
+            System.out.println("(5) Zurückgeben");
+            System.out.println("(0) Programm beenden");
+            System.out.print("Geben sie die gewünschte Option an: ");
+
+            Scanner sc = new Scanner(System.in);
+            int option = sc.nextInt();
+
+            switch (option) {
+                case 1:
+                    //Kunde Anlegen
+                    System.out.println("Kunde wurde erfasst!\n\n");
+                case 2:
+                    //Medium erfassen
+                    System.out.println("Medium wurde erfasst!\n\n");
+
+                case 3:
+                    //Ausleihen
+                    System.out.println("Ausleihe wurde erfasst!\n\n");
+                case 4:
+                    //Verlängern
+                    System.out.println("Verlängerung wurde erfasst!\n\n");
+
+                case 5:
+                    //Zurückgeben
+                    System.out.println("Rückgabe wurde erfasst!\n\n");
+
+                case 0:
+                    System.out.println("Vielen Dank für die Nutzung der Software, bis bald!\n\n");
+                    System.exit(0);
+                default:
+                    System.out.println("Option ist nicht bekannt!\n\n");
+            }
+        }
+
 
     }
 
